e73e50792572d91aefa38ce9ba6dfd39d488f048
unknown <Admin2@WIN-NAQ0FVOETRT>
2018-05-16 17:45:40 +0400

    LR script test

--- Action.c
+++ Action.c
@@ -4,5 +4,13 @@
 
 Action()
 {
+	truclient_step("1", "Navigate to 'http://google.com/'", "snapshot=Action_1.inf");
+	truclient_step("3", "search (1)", "snapshot=Action_3.inf");
+	{
+		truclient_step("3.1", "Click on ????? combobox", "snapshot=Action_3.1.inf");
+		truclient_step("3.2", "Type kek in ????? combobox", "snapshot=Action_3.2.inf");
+		truclient_step("3.3", "Press key Enter on ????? combobox", "snapshot=Action_3.3.inf");
+	}
+
 	return 0;
 }
