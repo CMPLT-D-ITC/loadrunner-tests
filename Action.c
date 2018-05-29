4c299e115f88da0f982bf527813b77fee76a4273
unknown <Admin2@WIN-NAQ0FVOETRT>
2018-05-29 19:20:29 +0400

    SADASDSAAS

--- Action.c
+++ Action.c
@@ -4,12 +4,12 @@
 
 Action()
 {
-	truclient_step("1", "Navigate to 'http://google.com/'", "snapshot=Action_1.inf");
+	truclient_step("1", "Navigate to 'http://google.comZ\XZ\XZ\XZ\Z\'", "snapshot=Action_1.inf");
 	truclient_step("3", "search (1)", "snapshot=Action_3.inf");
 	{
-		truclient_step("3.1", "Click on ????? combobox", "snapshot=Action_3.1.inf");
-		truclient_step("3.2", "Type kek in ????? combobox", "snapshot=Action_3.2.inf");
-		truclient_step("3.3", "Press key Enter on ????? combobox", "snapshot=Action_3.3.inf");
+		truclient_step("3.1", "Click on KDSAJFKJHDG combobox", "snapshot=Action_3.1.inf");
+		truclient_step("3.2", "Type kek in KDSAJFKJHDG combobox", "snapshot=Action_3.2.inf");
+		truclient_step("3.3", "Press key Enter on KDSAJFKJHDG combobox", "snapshot=Action_3.3.inf");
 	}
 
 	return 0;
